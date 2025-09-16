aae6fc1f69d2d0f0b2d80c0e0b511a21632ea855
bruuhyan <163632842+bruuhyan@users.noreply.github.com>
2025-09-16 11:30:21 +0700

    Commit No.4 – use jetpack navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
